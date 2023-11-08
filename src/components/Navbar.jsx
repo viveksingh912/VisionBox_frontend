87069f84c5d51901089701b29085564d9263f4a3
Vivek Singh <vivek.singh@codingninjas.com>
2023-11-09 01:28:06 +0530

    changed the hamburger col

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -91,8 +91,9 @@ const HamBurger = styled.div`
 const HamItem = styled.div`
   height: 3px;
   width: 20px;
+  background-color:${({theme})=>theme.text};
   background-color: white;
-  background: white;
+  background: ${({theme})=>theme.text};
   margin: 5px 0px;
 `;
 
